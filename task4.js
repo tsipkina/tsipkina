7f0be4c8183ec3f9ccec2e54e7cfb3b2af598650
tsipkina <97594716+tsipkina@users.noreply.github.com>
2022-01-12 16:26:35 +0200

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,3 +1,3 @@
 var a = 0;
 
-switch (a1) {case 0: console.log='0';break; case 1: console.log='1';break; case 2: case 3: console.log='2,3';}
+switch (a) {case 0: console.log='0';break; case 1: console.log='1';break; case 2: case 3: console.log='2,3';}
