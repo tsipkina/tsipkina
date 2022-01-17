5567238aa1b241729dc4dd8f167d18976a54ee4b
tsipkina <97594716+tsipkina@users.noreply.github.com>
2022-01-17 12:11:39 +0200

    Add files via upload

--- task3.js
+++ task3.js
@@ -1,8 +1,8 @@
 var val='d';
-if (val!='a' && val!='b' && val!='c' && val!='d' && val!='e')
- {console.log('unknown');} 
-else {
-    if (val=='a') console.log('a');
-    else
-    {console.log('others')} 
-}
+if (val==='a') {console.log ('a')} 
+else if (val==='b' || val==='c' || val==='d' || val==='e') 
+    {console.log('others')}
+     else {console.log('unknown');}
+
+
+  
