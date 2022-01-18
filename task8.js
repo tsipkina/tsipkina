08938fa4d0130d336380f7ee4288172babb57903
tsipkina <97594716+tsipkina@users.noreply.github.com>
2022-01-18 08:43:00 +0200

    Add files via upload

--- task8.js
+++ task8.js
@@ -1,5 +1,6 @@
 var arr = [1,2,3,4];
 var sum = 0;
-for(var i = 0; i < arr.length; i++)
+var len=arr.length;
+for(var i = 0; i < len; i++)
 { if (arr[i] % 2==0 && arr[i]>3) {sum += arr[i];} } 
 console.log(sum)
